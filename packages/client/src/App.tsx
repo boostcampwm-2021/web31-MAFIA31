1e6ea2f665e21c268f2001750ab63c653a65aeaf
donggoolosori <kdj6551@naver.com>
2021-10-27 16:20:10 +0900

    chore: eslint 룰 추가
    - tsx 사용가능
    - jsx에 React를 반드시 import 하지 않아도 됨

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const state = useState('hi');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{state}</span>
     </div>
   );
-}
+};
 
 export default App;

1464631703b644c3dadaa6de99b135ad2a60bdc7
donggoolosori <kdj6551@naver.com>
2021-11-03 15:37:42 +0900

    feat: 채팅 메시지 컴포넌트 추가

--- packages/client/src/components/Message/ChatMsg.tsx
+++ packages/client/src/components/Message/ChatMsg.tsx
@@ -1,3 +1,22 @@
-const ChatMsg = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { ChatMsgType } from '../../types/ChatTypes';
 
+interface PropType {
+  chat: ChatMsgType;
+  isMyMsg: boolean;
+}
+
+const msg = css`
+  padding: 12px;
+  color: red;
+`;
+
+const ChatMsg: FC<PropType> = ({ chat, isMyMsg }) => (
+  <div css={msg}>
+    {chat.msg}
+    {isMyMsg}
+  </div>
+);
 export default ChatMsg;

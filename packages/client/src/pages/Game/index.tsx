5b612b54087733171de6f02f7d7fc0a8c9086325
Kim Hyunjo <alsxmqkqh23@gmail.com>
2021-11-05 00:33:24 +0900

    refactor: 주석삭제

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,24 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { useRef } from 'react';
-// import { io, Socket } from 'socket.io-client';
 import { v4 as uuidv4 } from 'uuid';
 
 import ChatContainer from '../../containers/ChatContainer';
 import LeftSideContainer from '../../containers/LeftSideContainer';
 import useChat from '../../hooks/useChat';
-// import { User } from '../../../../domain/types/user';
 
 const Game = () => {
   const myName = useRef(uuidv4());
   const ROOM_ID = 'hi';
   const { chatList, sendChat } = useChat(ROOM_ID);
-  // const [userList, setUserList] = useState([myName]);
-
-  // useEffect(() => {
-  //   myName =;
-  //   console.log(myName);
-  // }, []);
 
   return (
     <div css={GamePageStyle}>

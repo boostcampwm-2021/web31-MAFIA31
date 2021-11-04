8eee8e1fabc049cd85efca9a2cb3e5c83e71c73f
dailyco <dailyco03@gmail.com>
2021-11-04 16:54:40 +0900

    feat: VoteContainer 추가 및 투표와 채팅 Container 레이아웃 수정

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,10 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+import VoteContainer from '../../containers/VoteContainer';
 import ChatContainer from '../../containers/ChatContainer';
 import useChat from '../../hooks/useChat';
 
 const Game = () => {
   const { chatList, sendChat } = useChat('hi');
 
-  return <ChatContainer chatList={chatList} sendChat={sendChat} />;
+  return (
+    <div css={gameContainerStyle}>
+      <VoteContainer />
+      <ChatContainer chatList={chatList} sendChat={sendChat} />
+    </div>
+  );
 };
+
+const gameContainerStyle = css`
+  display: flex;
+
+  height: 100%;
+`;
 
 export default Game;

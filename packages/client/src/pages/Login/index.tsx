339c88e0f859bb648027711f652ac1a39c6f4743
J078_민수빈 <tnqls5417@naver.com>
2021-11-05 01:23:21 +0900

    [TASK] 로그인 페이지 구현 (#29)
    * fix: 게임 소켓 연결시 투표 가능하게 변경
    
    - game start 이벤트 받지 않도록 수정
    - 10초간 투표 가능하도록 투표 가능 시간 조정
    
    * feat: 로그인 페이지 추가
    
    - 버튼 클릭시 /game으로 바로 이동하도록 구현
    - container height 900px로 임시 설정

--- packages/client/src/pages/Login/index.tsx
+++ packages/client/src/pages/Login/index.tsx
@@ -1,3 +1,60 @@
-const Login = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { Link } from 'react-router-dom';
+import { primaryDark, white } from '../../constants';
+
+const Login: FC = () => (
+  <div css={containerStyle}>
+    <div css={logoTextStyle}>MAFIA 31</div>
+    <img css={logoImageStyle} src="/assets/icons/home.png" alt="home" />
+    <Link to="/game">
+      <button css={buttonStyle} type="button">
+        <img css={iconImageStyle} src="/assets/icons/github.png" alt="github" /> Github 로그인
+      </button>
+    </Link>
+  </div>
+);
+
+const logoTextStyle = css`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 1000;
+  font-size: 100px;
+  line-height: 145px;
+  color: ${primaryDark};
+  margin-bottom: 80px;
+`;
+
+const logoImageStyle = css`
+  width: 400px;
+  height: 400px;
+  margin-bottom: 80px;
+`;
+
+const iconImageStyle = css`
+  margin-right: 16px;
+`;
+
+const containerStyle = css`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const buttonStyle = css`
+  display: flex;
+  align-items: center;
+  background-color: ${primaryDark};
+  justify-content: center;
+  padding: 12px;
+  width: 500px;
+  height: 75px;
+  border-radius: 15px;
+  font-size: 40px;
+  color: ${white};
+`;
 
 export default Login;

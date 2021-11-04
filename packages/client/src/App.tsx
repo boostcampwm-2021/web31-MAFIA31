913b7b0916451f532a358e2d4ae73ace54055b90
J078_민수빈 <tnqls5417@naver.com>
2021-11-05 01:23:21 +0900

    [TASK] 로그인 페이지 구현 (#29)
    * fix: 게임 소켓 연결시 투표 가능하게 변경
    
    - game start 이벤트 받지 않도록 수정
    - 10초간 투표 가능하도록 투표 가능 시간 조정
    
    * feat: 로그인 페이지 추가
    
    - 버튼 클릭시 /game으로 바로 이동하도록 구현
    - container height 900px로 임시 설정

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NotFound from './pages/Error/NotFound';
 import Game from './pages/Game';
 import GameResult from './pages/GameResult';
 import Login from './pages/Login';
@@ -10,13 +9,12 @@ import Waiting from './pages/Waiting';
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/login" component={Login} />
       <Route exact path="/profile" component={Profile} />
       <Route exact path="/rooms" component={Rooms} />
       <Route exact path="/waiting" component={Waiting} />
       <Route exact path="/game" component={Game} />
       <Route exact path="/game-result" component={GameResult} />
-      <Route component={NotFound} />
+      <Route component={Login} />
     </Switch>
   </Router>
 );

78aadd83461a4dca9b9707100143535294a87a31
Subin Min <tnqls5417@naver.com>
2021-11-04 14:54:39 +0900

    feat: 투표 시간 처리 및 투표 결과 계산 구현
    -  setTimeout 이용해서 startVoteTime의 time 동안만 투표 받기
    - 동표이거나 기권일시 execution의 username undefined
    - room들의 userName마다의 투표수 저장하는 VoteInfo type 추가

--- packages/server/loaders/sockets/game.ts
+++ packages/server/loaders/sockets/game.ts
@@ -1,10 +1,64 @@
 import { Namespace, Socket } from 'socket.io';
 import { Vote } from '../../../domain/types/vote';
 
+interface VoteInfo {
+  [key: string]: {
+    [key: string]: number;
+  };
+}
+
+const userInfo: string[] = ['user1', 'user2', 'user3'];
+const voteInfo: VoteInfo = {};
+let canVote: boolean = false;
+
+const publish = (namespace: Namespace, roomId: string) => {
+  let maxCnt = 0;
+  let maxName = userInfo[0];
+  let sameCnt = 0;
+
+  Object.keys(voteInfo[roomId]).forEach((userName) => {
+    if (voteInfo[roomId][userName] >= maxCnt) {
+      if (voteInfo[roomId][userName] === maxCnt) sameCnt += 1;
+      else {
+        maxCnt = voteInfo[roomId][userName];
+        maxName = userName;
+        sameCnt = 0;
+      }
+    }
+  });
+
+  namespace
+    .to(roomId)
+    .emit('execution', { userName: maxCnt === 0 || sameCnt > 0 ? undefined : maxName });
+};
+
+const startVoteTime = (namespace: Namespace, roomId: string, time: number) => {
+  canVote = true;
+
+  setTimeout(() => {
+    canVote = false;
+    publish(namespace, roomId);
+
+    userInfo.forEach((user) => {
+      voteInfo[roomId][user] = 0;
+    });
+  }, time);
+};
+
 const gameSocketInit = (namespace: Namespace, socket: Socket, roomId: string): void => {
+  voteInfo[roomId] = {};
+  userInfo.forEach((user) => {
+    voteInfo[roomId][user] = 0;
+  });
+
   socket.on('vote', (vote: Vote) => {
+    if (!canVote) return;
+
+    voteInfo[roomId][vote.to] += 1;
     namespace.to(roomId).emit('publish vote', vote);
   });
+
+  startVoteTime(namespace, roomId, 5000);
 };
 
 export default gameSocketInit;

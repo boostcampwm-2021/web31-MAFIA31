0ebf4cbd3096eaa727b3b7128ffee88a9f74aa28
YuJin Kim <48382813+dailyco@users.noreply.github.com>
2021-11-04 17:05:30 +0900

    [TASK] 투표 컴포넌트 UI 생성 및 투표 소켓 커스텀 훅 구현 (#26)
    * feat: User, Vote 타입 인터페이스 생성
    
    * feat: AbilityBtn 컴포넌트 UI 생성
    
    * feat: 클라이언트 투표 소켓 커스텀 훅 생성
    
    useVote 커스텀 훅을 생성하여 투표와 관련된 로직 처리
    
    * feat: VoteContainer 컴포넌트 UI 생성
    
    * feat: VoteContainer 추가 및 투표와 채팅 Container 레이아웃 수정
    
    Co-authored-by: Kim Hyunjo <63906230+Kim-Hyunjo@users.noreply.github.com>

---
+++ packages/client/src/containers/VoteContainer.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { AbilityBtn } from '../components/Button';
+import colors from '../constants/colors';
+import useVote from '../hooks/useVote';
+
+const VoteContainer: FC = () => {
+  const { playerList } = useVote('mafia31', 'hi');
+
+  return (
+    <div css={VoteContainerStyle}>
+      {playerList.map(({ userImg, nickname, voteCnt }) => (
+        <AbilityBtn key={nickname} userImg={userImg} nickName={nickname} voteCnt={voteCnt} />
+      ))}
+    </div>
+  );
+};
+
+const VoteContainerStyle = css`
+  display: flex;
+  background-color: ${colors.primaryDark};
+
+  padding: 40px;
+  width: 400px;
+  height: 100%;
+`;
+
+export default VoteContainer;

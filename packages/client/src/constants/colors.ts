7e7e90c76f9f02cc51816e1263b9128abaf21dab
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 03:20:53 +0900

    chore: colors에 grey1,2,3 추가

--- packages/client/src/constants/colors.ts
+++ packages/client/src/constants/colors.ts
@@ -2,3 +2,6 @@ export const primaryLight = '#F4F7FB';
 export const primaryDark = '#4E416D';
 export const white = '#FFFFFF';
 export const titleActive = '#3C3C3C';
+export const grey1 = '#D7D7D7';
+export const grey2 = '#BBBBBB';
+export const grey3 = '#D9DCE0';

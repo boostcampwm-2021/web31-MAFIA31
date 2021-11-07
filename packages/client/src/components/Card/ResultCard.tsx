e28e60e21d28cf3fc77d235fc4ccec01251cf336
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 04:23:38 +0900

    feat: 게임 결과 화면 생성
    - GameResult 페이지 생성
    - 간단한 레이아웃과 스타일 적용

--- packages/client/src/components/Card/ResultCard.tsx
+++ packages/client/src/components/Card/ResultCard.tsx
@@ -1,3 +1,29 @@
-const ResultCard = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { white } from '../../constants/colors';
+
+interface PropType {
+  userName: string;
+  job: string;
+  isWinner: boolean;
+}
+const ResultCard: FC<PropType> = ({ userName, job, isWinner }) => (
+  <div css={ResultCardStyle(isWinner)}>
+    <div>{job}</div>
+    <div>{userName}</div>
+  </div>
+);
+
+const ResultCardStyle = (isWinner: boolean) => css`
+  position: relative;
+  width: 190px;
+  height: 243px;
+
+  background: ${white};
+  box-shadow: 2px 2px 8px rgba(78, 65, 109, 0.25);
+  border-color: ${isWinner ? 'red' : 'none'};
+  border-radius: 20px;
+`;
 
 export default ResultCard;

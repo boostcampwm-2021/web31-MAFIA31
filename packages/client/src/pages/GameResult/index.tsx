8c102f4aa0cafe04e5a03590789fe1b943b89494
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 04:23:38 +0900

    feat: 게임 결과 화면 생성
    - GameResult 페이지 생성
    - 간단한 레이아웃과 스타일 적용

--- packages/client/src/pages/GameResult/index.tsx
+++ packages/client/src/pages/GameResult/index.tsx
@@ -1,3 +1,32 @@
-const GameResult = () => <div />;
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { primaryLight } from '../../constants/colors';
+import { ResultCard } from '../../components/Card';
+
+const GameResult = () => {
+  const temp = '';
+  return (
+    <div css={ResultPageStyle}>
+      <div>
+        <div>MAFIA WIN!</div>
+        <div>YOU LOSE</div>
+      </div>
+      <ResultCard userName="user1" job="mafia" isWinner={false} />
+      <div>{temp}</div>
+      <div>
+        <button type="button">돌아가기</button>
+        <button type="button">방 나가기</button>
+      </div>
+    </div>
+  );
+};
+
+const ResultPageStyle = css`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  background: ${primaryLight};
+`;
 
 export default GameResult;

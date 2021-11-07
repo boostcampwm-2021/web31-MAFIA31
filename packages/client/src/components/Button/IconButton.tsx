3587c680238ed85767825966a55301ad9897cac4
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 05:51:49 +0900

    feat: IconButton 작성
    - enum 오류로 인하여 해당 파일에 한하여 no-unused-vars 무시
    - line 10에 한하여 no-shadow 무시

--- packages/client/src/components/Button/IconButton.tsx
+++ packages/client/src/components/Button/IconButton.tsx
@@ -1,3 +1,54 @@
-const IconButton = () => <div />;
+/* eslint-disable no-unused-vars */
+/** @jsxImportSource @emotion/react */
+import { css, SerializedStyles } from '@emotion/react';
+
+type themeOptions = {
+  [key: string]: SerializedStyles;
+};
+
+// eslint-disable-next-line no-shadow
+export enum ButtonSizeList {
+  SMALL = 'SMALL',
+  MEDIUM = 'MEDIUM',
+  LARGE = 'LARGE',
+}
+
+interface Props {
+  size: ButtonSizeList;
+  color: string;
+  imageSrc: string;
+  text: string;
+  onClick: () => void;
+}
+
+const IconButton = ({ size, color, imageSrc, text, onClick }: Props) => (
+  <button css={buttonStyle(size, color)} type="button" onClick={onClick}>
+    <img src={imageSrc} alt="button" />
+    <p>{text}</p>
+  </button>
+);
+
+const buttonStyle = (size: string, color: string) => css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: ${buttonSizeStyle[size]};
+  background-color: ${color};
+`;
+
+const buttonSizeStyle: themeOptions = {
+  SMALL: css`
+    width: 150px;
+    height: 60px;
+  `,
+  MEDIUM: css`
+    width: 300px;
+    height: 80px;
+  `,
+  LARGE: css`
+    width: 500px;
+    height: 100px;
+  `,
+};
 
 export default IconButton;

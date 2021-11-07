0e7d550d495353b86e4f67e700d7a3b7c269f4e6
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 05:19:51 +0900

    feat: 결과페이지 승리 스탬프 애니메이션 추가
    - 승리 결과카드에 스탬프 추가
    - 위의 스탬프에 css 애니메이션 추가

--- packages/client/src/components/Card/ResultCard.tsx
+++ packages/client/src/components/Card/ResultCard.tsx
@@ -8,22 +8,49 @@ interface PropType {
   job: string;
   isWinner: boolean;
 }
-const ResultCard: FC<PropType> = ({ userName, job, isWinner }) => (
-  <div css={ResultCardStyle(isWinner)}>
-    <div>{job}</div>
-    <div>{userName}</div>
-  </div>
-);
+const ResultCard: FC<PropType> = ({ userName, job, isWinner }) => {
+  const WINNER_STAMP_SRC = '/assets/icons/stamp-winner.png';
+  return (
+    <div css={ResultCardStyle}>
+      <img css={WinnerStampStyle(isWinner)} src={WINNER_STAMP_SRC} alt="winner stamp" />
+      <div>{job}</div>
+      <div>{userName}</div>
+    </div>
+  );
+};
 
-const ResultCardStyle = (isWinner: boolean) => css`
+const ResultCardStyle = css`
   position: relative;
   width: 190px;
   height: 243px;
 
   background: ${white};
   box-shadow: 2px 2px 8px rgba(78, 65, 109, 0.25);
-  border-color: ${isWinner ? 'red' : 'none'};
   border-radius: 20px;
+`;
+
+const WinnerStampStyle = (isWinner: boolean) => css`
+  display: ${isWinner ? 'block' : 'none'};
+
+  animation-name: pulse;
+  animation-duration: 0.5s;
+  animation-fill-mode: forwards;
+
+  @keyframes pulse {
+    0% {
+      opacity: 0;
+    }
+    10% {
+      opacity: 0.5;
+      transform-origin: 50% 50%;
+      transform: rotate(0.2turn) scale(5);
+      transition: all 0.3s ease-out;
+    }
+    100% {
+      opacity: 1;
+      transform: rotate(-0.05turn) scale(1);
+    }
+  }
 `;
 
 export default ResultCard;

defeb55e78513fa09d8c38ea964c21c032a49629
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 03:38:26 +0900

    feat: 사망자 표시 구현
    - playerState를 array로 구현
    - game page에서 playerState 관리

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -7,15 +7,17 @@ import { primaryDark } from '../../constants/index';
 import ChatContainer from '../../containers/ChatContainer';
 import LeftSideContainer from '../../containers/LeftSideContainer';
 import useChat from '../../hooks/useChat';
+import useExecute from '../../hooks/useExecute';
 
 const Game = () => {
   const myName = useRef(uuidv4());
   const ROOM_ID = 'hi';
   const { chatList, sendChat } = useChat(ROOM_ID);
+  const playerStateList = useExecute(ROOM_ID);
 
   return (
     <div css={GamePageStyle}>
-      <LeftSideContainer roomId={ROOM_ID} />
+      <LeftSideContainer roomId={ROOM_ID} playerStateList={playerStateList} />
       <ChatContainer chatList={chatList} sendChat={sendChat} myName={myName.current} />
     </div>
   );

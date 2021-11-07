3b9c6cf3663ab2603d10cf138e5fb45f2cde5049
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 08:03:07 +0900

    feat: Image 컴포넌트 작성
    - 사이즈와 경로를 프로퍼티로 받음

--- packages/client/src/components/Image/index.tsx
+++ packages/client/src/components/Image/index.tsx
@@ -1,3 +1,42 @@
-const Image = () => <div />;
+/* eslint-disable no-unused-vars */
+/** @jsxImportSource @emotion/react */
+import { css, SerializedStyles } from '@emotion/react';
 
-export default Image;
+type themeOptions = {
+  [key: string]: SerializedStyles;
+};
+
+// eslint-disable-next-line no-shadow
+export enum ImageSizeList {
+  SMALL = 'SMALL',
+  MEDIUM = 'MEDIUM',
+  LARGE = 'LARGE',
+}
+
+interface Props {
+  size: ImageSizeList;
+  src: string;
+}
+
+export const Image = ({ size, src }: Props) => <img css={imageStyle(size)} src={src} alt="" />;
+
+const imageStyle = (size: string) => css`
+  box-shadow: 2px 2px 10px rgba(78, 65, 109, 0.25);
+  border-radius: 15px;
+  ${imageSizeStyle[size]}
+`;
+
+const imageSizeStyle: themeOptions = {
+  SMALL: css`
+    width: 80px;
+    height: 80px; ;
+  `,
+  MEDIUM: css`
+    width: 180px;
+    height: 180px;
+  `,
+  LARGE: css`
+    width: 300px;
+    height: 300px;
+  `,
+};

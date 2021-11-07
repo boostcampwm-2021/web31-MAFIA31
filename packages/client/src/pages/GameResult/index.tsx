d1e1026d2db9b7970e3e3ac0b88239df02e63286
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-08 06:14:11 +0900

    feat: 게임 결과 페이지에 IconButton 추가

--- packages/client/src/pages/GameResult/index.tsx
+++ packages/client/src/pages/GameResult/index.tsx
@@ -1,21 +1,35 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { primaryLight } from '../../constants/colors';
+import { primaryLight, white } from '../../constants/colors';
 import { ResultCard } from '../../components/Card';
+import { IconButton } from '../../components/Button';
+import { ButtonSizeList, ButtonThemeList } from '../../components/Button/IconButton';
 
 const GameResult = () => {
   const temp = '';
   return (
     <div css={ResultPageStyle}>
-      <div>
+      <div css={ResultTitleStyle}>
         <div>MAFIA WIN!</div>
         <div>YOU LOSE</div>
       </div>
       <ResultCard userName="user1" job="mafia" isWinner />
       <div>{temp}</div>
       <div>
-        <button type="button">돌아가기</button>
-        <button type="button">방 나가기</button>
+        <IconButton
+          size={ButtonSizeList.MEDIUM}
+          theme={ButtonThemeList.DARK}
+          imageSrc="/assets/icons/undo.png"
+          text="돌아가기"
+          onClick={() => {}}
+        />
+        <IconButton
+          size={ButtonSizeList.MEDIUM}
+          theme={ButtonThemeList.DARK}
+          imageSrc="/assets/icons/exit.png"
+          text="방 나가기"
+          onClick={() => {}}
+        />
       </div>
     </div>
   );
@@ -24,9 +38,20 @@ const GameResult = () => {
 const ResultPageStyle = css`
   display: flex;
   flex-direction: column;
+  align-items: center;
   height: 100vh;
 
   background: ${primaryLight};
+`;
+
+const ResultTitleStyle = css`
+  position: relative;
+  width: 1011px;
+  height: 181px;
+
+  background-color: ${white};
+  box-shadow: 2px 2px 8px rgba(78, 65, 109, 0.25);
+  border-radius: 20px;
 `;
 
 export default GameResult;

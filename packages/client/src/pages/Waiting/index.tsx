ba7193adc76520e1e4c81c35393be6ddc2a279fb
Subin Min <tnqls5417@naver.com>
2021-11-09 13:42:11 +0900

    feat: DefaultButton 구현
    - IconButton의 enum import 사용해서 구현
    - no shadow off 코드 삭제

--- packages/client/src/pages/Waiting/index.tsx
+++ packages/client/src/pages/Waiting/index.tsx
@@ -1,3 +1,14 @@
-const Waiting = () => <div />;
+import { DefaultButton } from '@src/components/Button';
+import { ButtonSizeList, ButtonThemeList } from '@src/components/Button/IconButton';
+import Header from '@src/templates/Header';
 
+const Waiting = () => {
+  console.log(ButtonSizeList.MEDIUM);
+  return (
+    <>
+      <Header />
+      <DefaultButton text="READY" size={ButtonSizeList.MEDIUM} theme={ButtonThemeList.LIGHT} />
+    </>
+  );
+};
 export default Waiting;

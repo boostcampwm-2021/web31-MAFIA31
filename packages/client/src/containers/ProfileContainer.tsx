c07206724afebfd397ae9a8cb7e9ff3441b0611d
Subin Min <tnqls5417@naver.com>
2021-11-10 04:17:49 +0900

    feat: ProfileContainer 방장 표시 추가
    - host userName과 같으면 host.png 이미지 추가
    - 방장 아이콘 position: absolute
    - ProfileCard와 아이콘 wrapper position: relative

--- packages/client/src/containers/ProfileContainer.tsx
+++ packages/client/src/containers/ProfileContainer.tsx
@@ -12,13 +12,20 @@ interface Prop {
 const ProfileContainer: FC<Prop> = ({ userList, host }) => (
   <div css={containerStyle}>
     {userList.map(({ userName }) => (
-      <ProfileCard
-        key={userName}
-        userName={userName}
-        status={userName === host ? 'WAITING' : 'READY'}
-        profileImg="assets/images/mafia.png"
-        fill="true"
-      />
+      <div css={wrapperStyle}>
+        <img
+          css={decoStyle[host === userName ? 'true' : 'false']}
+          src="\assets\icons\host.png"
+          alt="host"
+        />
+        <ProfileCard
+          key={userName}
+          userName={userName}
+          status={userName === host ? 'WAITING' : 'READY'}
+          profileImg="assets/images/mafia.png"
+          fill="true"
+        />
+      </div>
     ))}
   </div>
 );
@@ -28,8 +35,22 @@ const containerStyle = css`
   grid-template-columns: repeat(4, 1fr);
   row-gap: 55px;
   column-gap: 40px;
-
   width: 920px;
 `;
+
+const wrapperStyle = css`
+  position: relative;
+`;
+
+const decoStyle = {
+  true: css`
+    position: absolute;
+    right: -30px;
+    top: -30px;
+  `,
+  false: css`
+    display: none;
+  `,
+};
 
 export default ProfileContainer;

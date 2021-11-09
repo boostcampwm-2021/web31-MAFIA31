3dee04f3cdc4fd58bb0247b4132e2a63323e4652
Subin Min <tnqls5417@naver.com>
2021-11-10 02:42:17 +0900

    feat: ProfileContainer 추가 및 Waiting 페이지 수정
    - css grid 사용해 4열 컨테이너 구현
    - userList와 방장의 userName인 host 받는 ProfileContainer 구현

---
+++ packages/client/src/containers/ProfileContainer.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { User } from 'domain/types/user';
+import { ProfileCard } from '@src/components/Card';
+
+interface Prop {
+  userList: User[];
+  host: string;
+}
+
+const ProfileContainer: FC<Prop> = ({ userList, host }) => (
+  <div css={containerStyle}>
+    {userList.map(({ userName }) => (
+      <ProfileCard
+        key={userName}
+        userName={userName}
+        status={userName === host ? 'WAITING' : 'READY'}
+        profileImg="assets/images/mafia.png"
+        fill="true"
+      />
+    ))}
+  </div>
+);
+
+const containerStyle = css`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  width: 920px;
+`;
+
+export default ProfileContainer;
--- packages/client/src/pages/Waiting/index.tsx
+++ packages/client/src/pages/Waiting/index.tsx
@@ -1,14 +1,32 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { useLocation, Link } from 'react-router-dom';
 import { DefaultButton } from '@components/Button';
 import { ButtonSizeList, ButtonThemeList } from '@components/Button/IconButton';
 import Header from '@src/templates/Header';
 import { RoomInfo } from '@src/types';
 import useSocket from '@hooks/useSocket';
+import { User } from 'domain/types/user';
+import ProfileContainer from '@src/containers/ProfileContainer';
 
 interface locationType {
   roomInfo: RoomInfo;
 }
-
+const dummyData: User[] = [
+  { userName: 'user1' },
+  { userName: 'user2' },
+  { userName: 'user3' },
+  { userName: 'user4' },
+  { userName: 'user5' },
+  { userName: 'user6' },
+  { userName: 'user7' },
+  { userName: 'user8' },
+  { userName: 'user9' },
+  { userName: 'user10' },
+  { userName: 'user11' },
+  { userName: 'user12' },
+];
+const host = 'user1';
 const Waiting = () => {
   const location = useLocation<locationType>();
   const { roomId } = location.state.roomInfo;
@@ -16,13 +34,20 @@ const Waiting = () => {
   console.log(socketRef); // TODO: socket을 useContext로 관리, 여기서 할당해주기!
 
   return (
-    <>
+    <div css={pageStyle}>
       <Header />
+      <ProfileContainer userList={dummyData} host={host} />
       <Link to="/game">
         <DefaultButton text="READY" size={ButtonSizeList.MEDIUM} theme={ButtonThemeList.LIGHT} />
       </Link>
-    </>
+    </div>
   );
 };
+
+const pageStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default Waiting;

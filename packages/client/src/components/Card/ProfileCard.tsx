14ee3152df63e40fb2c8181f4c75589709eaaeac
Subin Min <tnqls5417@naver.com>
2021-11-10 02:38:44 +0900

    feat: ProfileCard 구현
    - 이름, 프로필 이미지, 상태 텍스트, 채움 여부 받는 ProfileCard 컴포넌트 구현
    - fill 여부 'true', 'false' string으로 판단

--- packages/client/src/components/Card/ProfileCard.tsx
+++ packages/client/src/components/Card/ProfileCard.tsx
@@ -1,3 +1,69 @@
-const ProfileCard = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { primaryDark, white } from '@src/constants';
+
+interface Prop {
+  userName: string;
+  profileImg: string;
+  status: string;
+  fill: 'true' | 'false';
+}
+
+const ProfileCard: FC<Prop> = ({ userName, profileImg, status, fill }) => (
+  <div css={cardStyle}>
+    <div css={textStyle}>{userName}</div>
+    <img css={imgStyle} src={profileImg} alt="profileImg" />
+    <div css={statusStyle(fill)}>{status}</div>
+  </div>
+);
+
+const cardStyle = css`
+  width: 200px;
+  height: 220px;
+  border-radius: 15px;
+  box-shadow: 4px 4px 4px rgba(78, 65, 109, 0.25);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
+
+const imgStyle = css`
+  width: 100px;
+  height: 100px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+const textStyle = css`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 23px;
+`;
+
+const statusStyle = (fill: 'true' | 'false') => css`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 35px;
+  ${fillStyle[fill]}
+`;
+
+const fillStyle = {
+  true: css`
+    color: ${primaryDark};
+    text-shadow: ${primaryDark};
+  `,
+  false: css`
+    color: ${white};
+    text-shadow: ${primaryDark};
+  `,
+};
 
 export default ProfileCard;

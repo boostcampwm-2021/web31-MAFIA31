a4465654dfae5b6f9c139269e0fdd1855bb726b9
Subin Min <tnqls5417@naver.com>
2021-11-10 03:43:43 +0900

    feat: Header 기본 구현
    - Light 테마 기준으로 색상 설정

--- packages/client/src/templates/Header/index.tsx
+++ packages/client/src/templates/Header/index.tsx
@@ -1,3 +1,29 @@
-const Header = () => <div />;
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { primaryDark, white } from '@src/constants';
+
+const Header = () => (
+  <div css={headerStyle}>
+    <div css={logoStyle}>MAFIA 31</div>
+  </div>
+);
+
+const headerStyle = css`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 140px;
+  background-color: ${primaryDark};
+`;
+
+const logoStyle = css`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 72px;
+  color: ${white};
+  margin-left: 40px;
+`;
 
 export default Header;

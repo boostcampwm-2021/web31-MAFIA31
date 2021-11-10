8a364f76245d4b49ac9b4e1bb211d12f961d282c
Kim Hyunjo <63906230+Kim-Hyunjo@users.noreply.github.com>
2021-11-10 23:45:54 +0900

    [TASK] 업적 페이지 UI 구현 (#65)
    * feat: header template 생성
    
    * fix: DefaultButton 수정
    
    - DefaultButton은 텍스트 가지고 선택적으로 아이콘 가질 수 있음
    - IconButton은 아이콘만 가짐
    
    * fix: ButtonSizeList, ButtonThemeList 위치를
    Button > index로 변경
    
    - 현재 default, icon button에서 모두 사용됨
    - 외부에서도 import됨
    - 해당 enum 두개를 Button component folder에서 바로 import할 수 있도록 수정
    
    * chore: 트로피 이미지 추가
    
    * chore: 업적 리스트 더미 데이터 추가
    
    * chore: Image 수정
    
    - SMALL_MEDIUM(120*120)사이즈 추가 (업적 페이지 사용)
    - 이미지 백그라운드 컬러 white로 지정
    
    * chore: grey4 color 추가
    
    - 업적 페이지 달성률 바에 사용
    
    * feat: AchievementCard 구현
    
    - achievement type(interface) domain에 추가 (title, imgSrc, isAccomplished)
    - 달성된 업적일 경우 opacity 50%로 설정 (추후 수정 가능)
    
    * feat: 업적 페이지 구현
    
    - 업적 달성률 바 css animation
    - 업적 리스트 overflow-y: scroll
    - 업적 리스트 렌더링 시 key: 업적 title
    - 업적 페이지 width 1220px 이하인 경우 레이아웃 깨지므로
    media query 사용하여 flex-direction: column 지정함
    
    * feat: 업적카드에 설명 추가
    
    - 업적에 대한 설명 추가
    
    * feat: 업적 카드 설명에 이벤트 추가
    
    - 업적 카드에 mouse올라오면 설명 보여줌
    - 구체적인 애니메이션 및 디자인 추후 변경 예정

--- packages/client/src/templates/Header/index.tsx
+++ packages/client/src/templates/Header/index.tsx
@@ -1,29 +1,52 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { DefaultButton, ButtonSizeList, ButtonThemeList } from '@src/components/Button';
 import { primaryDark, white } from '@src/constants';
 
-const Header = () => (
-  <div css={headerStyle}>
-    <div css={logoStyle}>MAFIA 31</div>
-  </div>
+const TITLE = 'MAFIA31';
+const CREATE_ROOM_BUTTON = '방 만들기';
+const MY_PAGE_BUTTON = '마이 페이지';
+
+const Header: FC = () => (
+  <header css={headerStyle}>
+    <h1>{TITLE}</h1>
+    <div css={buttonWrapperStyle}>
+      <DefaultButton
+        text={CREATE_ROOM_BUTTON}
+        size={ButtonSizeList.SMALL}
+        theme={ButtonThemeList.DARK}
+      />
+      <Link to={{ pathname: '/profile' }}>
+        <DefaultButton
+          text={MY_PAGE_BUTTON}
+          size={ButtonSizeList.SMALL}
+          theme={ButtonThemeList.DARK}
+        />
+      </Link>
+    </div>
+  </header>
 );
 
 const headerStyle = css`
   display: flex;
   align-items: center;
-  width: 100%;
-  height: 120px;
-  background-color: ${primaryDark};
-`;
+  justify-content: space-between;
+  padding: 40px;
 
-const logoStyle = css`
-  font-family: Noto Sans KR;
-  font-style: normal;
+  height: 140px;
+
   font-weight: bold;
   font-size: 50px;
   line-height: 72px;
   color: ${white};
-  margin-left: 40px;
+  background-color: ${primaryDark};
+`;
+
+const buttonWrapperStyle = css`
+  display: flex;
+  gap: 10px;
 `;
 
 export default Header;

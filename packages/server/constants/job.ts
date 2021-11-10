e14cd0f0b8f69016c32905d6baa3c24de20bd3e4
dailyco <dailyco03@gmail.com>
2021-11-10 18:10:33 +0900

    feat: 랜덤 직업 할당 구현
    - 랜덤 직업 할당을 위한 직업 배정 수 정의
    - 랜덤 직업 할당 논리 구현

---
+++ packages/server/constants/job.ts
@@ -0,0 +1,26 @@
+const createJobCountArr = ({ mafia = 0, citizen = 0, police = 0, doctor = 0 }): string[] => [
+  ...new Array(mafia).fill('mafia'),
+  ...new Array(citizen).fill('citizen'),
+  ...new Array(police).fill('police'),
+  ...new Array(doctor).fill('doctor'),
+];
+
+const JOB_COUNT: object[] = [
+  {},
+  {},
+  {},
+  {},
+  { mafia: 1, citizen: 4 },
+  { mafia: 2, citizen: 4 },
+  { mafia: 2, citizen: 5 },
+  { mafia: 2, citizen: 6 },
+  { mafia: 3, citizen: 5 },
+  { mafia: 3, citizen: 6 },
+  { mafia: 3, citizen: 7 },
+  { mafia: 3, citizen: 8 },
+  { mafia: 4, citizen: 8 },
+];
+
+const JOB_ARR: string[][] = JOB_COUNT.map((cnt) => createJobCountArr(cnt));
+
+export { JOB_COUNT, JOB_ARR };

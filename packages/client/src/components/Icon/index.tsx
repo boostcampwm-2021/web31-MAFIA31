2b2f29c8fd904eed6e92443888a604963b0693e3
dailyco <dailyco03@gmail.com>
2021-11-10 16:52:10 +0900

    refactor: ChatContainer 컴포넌트 리팩토링
    - 채팅 보내는 버튼 IconButton 적용

--- packages/client/src/components/Icon/index.tsx
+++ packages/client/src/components/Icon/index.tsx
@@ -1,2 +1,4 @@
 export { ReactComponent as SettingIcon } from './setting.svg';
 export { ReactComponent as RoomOutIcon } from './room-out.svg';
+export { ReactComponent as VoteIcon } from './vote.svg';
+export { ReactComponent as SendIcon } from './send.svg';

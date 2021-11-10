02ae4138dedbcb2f09027df3e6c28e94c1ce558c
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-10 17:29:39 +0900

    chore: Image 수정
    - SMALL_MEDIUM(120*120)사이즈 추가 (업적 페이지 사용)
    - 이미지 백그라운드 컬러 white로 지정

--- packages/client/src/components/Image/index.tsx
+++ packages/client/src/components/Image/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /** @jsxImportSource @emotion/react */
 import { css, SerializedStyles } from '@emotion/react';
+import { white } from '@src/constants';
 
 type themeOptions = {
   [key: string]: SerializedStyles;
@@ -9,6 +10,7 @@ type themeOptions = {
 // eslint-disable-next-line no-shadow
 export enum ImageSizeList {
   SMALL = 'SMALL',
+  SMALL_MEDIUM = 'SMALL_MEDIUM',
   MEDIUM = 'MEDIUM',
   LARGE = 'LARGE',
 }
@@ -21,6 +23,7 @@ interface Props {
 export const Image = ({ size, src }: Props) => <img css={imageStyle(size)} src={src} alt="" />;
 
 const imageStyle = (size: string) => css`
+  background-color: ${white};
   box-shadow: 2px 2px 10px rgba(78, 65, 109, 0.25);
   border-radius: 15px;
   ${imageSizeStyle[size]}
@@ -30,6 +33,10 @@ const imageSizeStyle: themeOptions = {
   SMALL: css`
     width: 80px;
     height: 80px; ;
+  `,
+  SMALL_MEDIUM: css`
+    width: 120px;
+    height: 120px;
   `,
   MEDIUM: css`
     width: 180px;

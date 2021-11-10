c04eb7a5e2a26438a5019b0203f5264282a4da6f
dailyco <dailyco03@gmail.com>
2021-11-11 00:22:45 +0900

    fix: Header export 추가 및 Rooms 페이지에 추가

--- packages/client/src/pages/Rooms/index.tsx
+++ packages/client/src/pages/Rooms/index.tsx
@@ -2,11 +2,15 @@ import { FC } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import RoomContainer from '@src/containers/RoomContainer';
+import Header from '@src/templates/Header';
 
 const Rooms: FC = () => (
-  <div css={roomPageStyle}>
-    <div css={padding}>
-      <RoomContainer />
+  <div>
+    <Header />
+    <div css={roomPageStyle}>
+      <div css={padding}>
+        <RoomContainer />
+      </div>
     </div>
   </div>
 );

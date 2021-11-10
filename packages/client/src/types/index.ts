ead5f0d78e1dcfee6ecf3785aa7d5a7b63797d21
Subin Min <tnqls5417@naver.com>
2021-11-10 11:08:22 +0900

    feat: WaitingInfo 타입 추가
    - WaitingInfo Waiting 페이지에서 방장 및 준비 상태 표시에 사용 (client types)
    - interface User 확장하도록 수정

--- packages/client/src/types/index.ts
+++ packages/client/src/types/index.ts
@@ -1,11 +1,17 @@
+import { User } from 'domain/types/user';
+
 export interface RoomInfo {
   roomId: string;
   title: string;
   host: string;
 }
 
-export interface PlayerInfo {
+export interface PlayerInfo extends User {
   userImg: string;
-  userName: string;
   voteFrom: string[];
+}
+
+export interface WaitingInfo extends User {
+  isHost: boolean;
+  isReady: boolean;
 }

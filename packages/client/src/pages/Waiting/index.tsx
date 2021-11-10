dbcca7a7c7c64276496b611cfb9f565979097699
J078_민수빈 <tnqls5417@naver.com>
2021-11-10 23:21:47 +0900

    [TASK] 대기방 페이지 UI (#63)
    * feat: ProfileCard 구현
    
    - 이름, 프로필 이미지, 상태 텍스트, 채움 여부 받는 ProfileCard 컴포넌트 구현
    - fill 여부 'true', 'false' string으로 판단
    
    * feat: ProfileContainer 추가 및 Waiting 페이지 수정
    
    - css grid 사용해 4열 컨테이너 구현
    - userList와 방장의 userName인 host 받는 ProfileContainer 구현
    
    * feat: Waiting 페이지 스크롤 및 하단 바 구현
    
    - ProfileContainer row-gap, column-gap 설정
    - Waiting  페이지 overflow scroll 설정
    - Waiting 페이지 bottomBar position fixed 설정
    
    * feat: Header 기본 구현
    
    - Light 테마 기준으로 색상 설정
    
    * feat: ProfileCard 추가 구현
    
    - 이미지 배경(wrapper) 추가
    - 텍스트 스트로크 설정
    
    * feat: ProfileContainer 방장 표시 추가
    
    - host userName과 같으면 host.png 이미지 추가
    - 방장 아이콘 position: absolute
    - ProfileCard와 아이콘 wrapper position: relative
    
    * fix: Waiting 페이지 수정
    
    - bottomBar position:fixed 제거
    
    * feat: WaitingInfo 타입 추가
    
    - WaitingInfo Waiting 페이지에서 방장 및 준비 상태 표시에 사용 (client types)
    - interface User 확장하도록 수정
    
    * refactor: css 적용 방식 수정
    
    - WaitingInfo 사용하도록 변경
    - 'true', 'false' string -> boolean 사용해서 css 다르게 적용하도록 수정

--- packages/client/src/pages/Waiting/index.tsx
+++ packages/client/src/pages/Waiting/index.tsx
@@ -1,12 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { useLocation, Link } from 'react-router-dom';
 import { DefaultButton, ButtonSizeList, ButtonThemeList } from '@components/Button';
 import Header from '@src/templates/Header';
-import { RoomInfo } from '@src/types';
+import { WaitingInfo, RoomInfo } from '@src/types';
 import useSocket from '@hooks/useSocket';
+import { white } from '@src/constants';
+import WaitingListContainer from '@src/containers/WaitingListContainer';
 
 interface locationType {
   roomInfo: RoomInfo;
 }
+
+const dummyData: WaitingInfo[] = [
+  { userName: 'user1', isHost: true, isReady: true },
+  { userName: 'user2', isHost: false, isReady: true },
+  { userName: 'user3', isHost: false, isReady: false },
+  { userName: 'user4', isHost: false, isReady: true },
+  { userName: 'user5', isHost: false, isReady: true },
+  { userName: 'user6', isHost: false, isReady: false },
+  { userName: 'user7', isHost: false, isReady: false },
+  { userName: 'user8', isHost: false, isReady: true },
+  { userName: 'user9', isHost: false, isReady: true },
+  { userName: 'user10', isHost: false, isReady: true },
+  { userName: 'user11', isHost: false, isReady: true },
+  { userName: 'user12', isHost: false, isReady: false },
+];
 
 const Waiting = () => {
   const location = useLocation<locationType>();
@@ -17,11 +36,58 @@ const Waiting = () => {
   return (
     <>
       <Header />
-      <Link to="/game">
-        <DefaultButton text="READY" size={ButtonSizeList.MEDIUM} theme={ButtonThemeList.LIGHT} />
-      </Link>
+      <div css={pageStyle}>
+        <div css={scrollStyle}>
+          <div css={marginStyle}>
+            <WaitingListContainer userList={dummyData} />
+          </div>
+        </div>
+        <div css={bottomBarStyle}>
+          <Link to="/game">
+            <DefaultButton
+              text="READY"
+              size={ButtonSizeList.MEDIUM}
+              theme={ButtonThemeList.LIGHT}
+            />
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
+
+const pageStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const scrollStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 90%;
+  height: 600px;
+
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const marginStyle = css`
+  margin-top: 40px;
+  margin-bottom: 40px;
+`;
+
+const bottomBarStyle = css`
+  width: 100%;
+  height: 140px;
+  background-color: ${white};
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-left: 70%;
+`;
 
 export default Waiting;

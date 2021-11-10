134d286e9ca69aedfbe0fec7275b3f811ca45863
donggoolosori <kdj6551@naver.com>
2021-11-10 18:44:30 +0900

    feat: 방 목록 추가 api 구현

--- packages/server/api/room/index.ts
+++ packages/server/api/room/index.ts
@@ -4,5 +4,6 @@ import RoomController from './room.controller';
 const roomRouter = express.Router();
 
 roomRouter.get('/', RoomController.getRoomList);
+roomRouter.post('/', RoomController.addRoom);
 
 export default roomRouter;
--- packages/server/api/room/room.controller.ts
+++ packages/server/api/room/room.controller.ts
@@ -1,11 +1,27 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import Room, { IRoom } from '../../models/Room';
 
 dotenv.config();
 
 const RoomController = {
   async getRoomList(req: express.Request, res: express.Response) {
-    console.log(req, res);
+    try {
+      const roomList = await Room.find();
+      res.json({ roomList });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async addRoom(req: express.Request, res: express.Response) {
+    try {
+      const newRoom: IRoom = req.body;
+      await Room.create(newRoom);
+
+      res.json(newRoom);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 

677a0203dfd76ac05306cb8ee14e486e0cf2983f
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-10 18:41:42 +0900

    feat: 업적 카드 설명에 이벤트 추가
    - 업적 카드에 mouse올라오면 설명 보여줌
    - 구체적인 애니메이션 및 디자인 추후 변경 예정

--- packages/client/src/components/Card/AchievementCard.tsx
+++ packages/client/src/components/Card/AchievementCard.tsx
@@ -1,20 +1,36 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { Achievement } from 'domain/types/achievement';
+import { grey1 } from '@src/constants/colors';
 import { Image, ImageSizeList } from '../Image';
 
-const AchievementCard: FC<Achievement> = ({ title, imgSrc, isAccomplished }) => (
-  <div css={achievementCardStyle}>
-    <Image size={ImageSizeList.SMALL_MEDIUM} src={imgSrc} css={imageStyle(isAccomplished)} />
-    <div>{title}</div>
-  </div>
-);
+const AchievementCard: FC<Achievement> = ({ title, imgSrc, isAccomplished, description }) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  return (
+    <div
+      css={achievementCardStyle}
+      onMouseOver={() => setIsMouseOver(true)}
+      onMouseOut={() => setIsMouseOver(false)}
+      onFocus={() => {}}
+      onBlur={() => {}}
+    >
+      <div css={imageWrapperStyle}>
+        <Image size={ImageSizeList.SMALL_MEDIUM} src={imgSrc} css={imageStyle(isAccomplished)} />
+        <p css={descriptionStyle(isMouseOver)}>{description}</p>
+      </div>
+      <div>{title}</div>
+    </div>
+  );
+};
 const achievementCardStyle = css`
   display: flex;
   flex-direction: column;
   text-align: center;
   gap: 8px;
+
+  width: fit-content;
+  height: fit-content;
 
   font-family: Noto Sans KR;
   font-style: normal;
@@ -22,9 +38,24 @@ const achievementCardStyle = css`
   font-size: 20px;
   line-height: 29px;
 `;
+const imageWrapperStyle = css`
+  position: relative;
+`;
 
 const imageStyle = (isAccomplished: boolean) => css`
+  position: relative;
+  z-index: 0;
   opacity: ${isAccomplished ? '50%' : '100%'};
+`;
+
+const descriptionStyle = (isMouseOver: boolean) => css`
+  position: absolute;
+  z-index: ${isMouseOver ? 1 : -1};
+  background-color: ${grey1};
+  width: 120px;
+  height: 120px;
+  text-align: center;
+  border-radius: 20px;
 `;
 
 export default AchievementCard;

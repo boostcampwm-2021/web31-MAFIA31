83bce6275cd7187e1a18bef8222071e27efd1447
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-11 18:50:16 +0900

    chore: socketId가 useAbility로 보내주기 위해 useSocket에 추가

--- packages/client/src/hooks/useSocket.ts
+++ packages/client/src/hooks/useSocket.ts
@@ -4,16 +4,16 @@ import { io, Socket } from 'socket.io-client';
 const useSocket = (roomId: string) => {
   const socketRef = useRef<Socket>();
   const SOCKET_URL: string = process.env.REACT_APP_SOCKET_URL || 'localhost:5001/';
+  const socketId = socketRef.current?.id;
 
   useEffect(() => {
     socketRef.current = io(SOCKET_URL + roomId);
-
     return () => {
       socketRef.current!.disconnect();
     };
   }, [roomId]);
 
-  return { socketRef };
+  return { socketRef, socketId };
 };
 
 export default useSocket;

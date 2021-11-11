6f20373c971face6657a8def7d763aa3d21f7b8d
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-11 18:44:32 +0900

    feat: Game 페이지에서 TURN_CHANGE 이벤트 듣기
    - isNight state 변경

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -5,23 +5,44 @@ import useSocket from '@hooks/useSocket';
 import useExecute from '@hooks/useExecute';
 import useVote from '@hooks/useVote';
 import useChat from '@hooks/useChat';
+import useAbility from '@src/hooks/useAbility';
 import { primaryDark } from '@constants/index';
 import ChatContainer from '@containers/ChatContainer';
 import LeftSideContainer from '@containers/LeftSideContainer';
 import RightSideContainer from '@containers/RightSideContainer';
+import { useEffect, useState } from 'react';
+import { TURN_CHANGE } from 'domain/constants/event';
 
 const Game = () => {
-  const { socketRef } = useSocket('123e4567-e89b-12d3-a456-426614174000');
+  const myUserName = 'user1';
+  const [isNight, setIsNight] = useState(false);
+  const { socketRef, socketId } = useSocket('123e4567-e89b-12d3-a456-426614174000');
   const playerStateList = useExecute(socketRef);
   const { chatList, sendChat } = useChat(socketRef);
-  const { playerList, voteUser } = useVote(socketRef, 'user1');
+  const { playerList, voteUser } = useVote(socketRef, myUserName);
+  const { emitAbility, mafiaPickList } = useAbility(socketRef, socketId!, 'user1', 'mafia');
+
+  useEffect(() => {
+    socketRef.current?.on(TURN_CHANGE, (isNight) => {
+      setIsNight(isNight);
+    });
+
+    return () => {
+      socketRef.current?.off(TURN_CHANGE);
+    };
+  }, [socketRef.current]);
 
   return (
     <div css={GamePageStyle}>
       <LeftSideContainer
         playerStateList={playerStateList}
         playerList={playerList}
+        myUserName={myUserName}
         voteUser={voteUser}
+        emitAbility={emitAbility}
+        mafiaPickList={mafiaPickList}
+        isNight={isNight}
+        socketRef={socketRef}
       />
       <ChatContainer chatList={chatList} sendChat={sendChat} />
       <RightSideContainer playerStateList={playerStateList} />

2ae2e683019a80af2e7cb08a774c2e021c510962
Subin Min <tnqls5417@naver.com>
2021-11-15 17:23:23 +0900

    feat: 스토리 메세지 추가

--- packages/client/src/components/Message/StoryMsg.tsx
+++ packages/client/src/components/Message/StoryMsg.tsx
@@ -1,3 +1,30 @@
-const StoryMsg = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { primaryDark } from '@src/constants';
+
+interface PropType {
+  msg: string;
+  imgSrc: string;
+}
+
+const StoryMsg: FC<PropType> = ({ msg, imgSrc }) => (
+  <div css={StoryMsgStyle}>
+    <div>{msg}</div>
+    <img src={imgSrc} alt="story" />
+  </div>
+);
+
+const StoryMsgStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 33px 60px;
+  width: 494px;
+  height: 325px;
+  background: ${primaryDark};
+  border-radius: 20px;
+`;
 
 export default StoryMsg;

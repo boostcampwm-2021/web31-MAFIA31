cb41b8013ef01fa4a4c2c46c940efee37d34bce8
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-15 18:21:01 +0900

    feat: story 관련 타입 추가
    - Story, StoryDictionary
    - 클라이언트에서 사용하는 MessageClient

--- packages/client/src/types/index.ts
+++ packages/client/src/types/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { Message } from '@mafia/domain/types/chat';
 import { User } from '@mafia/domain/types/user';
 
 export interface RoomInfo {
@@ -9,4 +11,24 @@ export interface RoomInfo {
 export interface PlayerInfo extends User {
   userImg: string;
   voteFrom: string[];
+}
+
+export interface MessageClient extends Message {
+  isStory: boolean;
+  imgSrc: string;
+}
+
+export interface Story {
+  id: string;
+  msg: string;
+  imgSrc: string;
+  isStory: boolean;
+  userName?: string;
+}
+
+export interface StoryDictionary {
+  [story: string]: {
+    msg: (name: string) => string;
+    imgSrc: string;
+  };
 }

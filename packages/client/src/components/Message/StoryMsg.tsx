74000b4f610254938ec4fcd926d6efbf5cdfce44
J078_민수빈 <tnqls5417@naver.com>
2021-11-15 21:46:27 +0900

    [TASK] 투표 결과 스토리 메세지로 연결 (#87)
    * feat: 스토리 메세지 추가
    
    * feat: Story 타입 추가 및 chatList에 적용
    
    - Message[] -> (Message | Story)[]
    - 타입 가드 위한 isStory() 함수 추가
    - chat에서 EXECUTION 이벤트 받아서 스토리 메시지 추가
    - 클라이언트 내 Story 인터페이스 추가
    
    * feat: ImageSizeList에 STORY 옵션 추가
    
    - 스토리일시 image 100% -> contain으로 변경
    - 스토리에 맞는 width, height 추가

--- packages/client/src/components/Message/StoryMsg.tsx
+++ packages/client/src/components/Message/StoryMsg.tsx
@@ -1,3 +1,36 @@
-const StoryMsg = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { primaryDark, white } from '@src/constants';
+import { Image, ImageSizeList } from '@components/Image';
+
+interface PropType {
+  msg: string;
+  imgSrc: string;
+}
+
+const StoryMsg: FC<PropType> = ({ msg, imgSrc }) => (
+  <div css={StoryMsgStyle}>
+    <div>{msg}</div>
+    <Image size={ImageSizeList.STORY} src={imgSrc} />
+  </div>
+);
+
+const StoryMsgStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 33px 60px;
+  width: 494px;
+  height: 325px;
+  background: ${primaryDark};
+  color: ${white};
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 35px;
+  border-radius: 20px;
+`;
 
 export default StoryMsg;

e41facb27b6c06a96b9f7367736a4c486e21c9fa
donggoolosori <kdj6551@naver.com>
2021-11-15 14:43:26 +0900

    feat: socket context 생성

---
+++ packages/client/src/contexts/socket.tsx
@@ -0,0 +1,26 @@
+import { createContext, FC, ReactNode, useContext, useRef } from 'react';
+import { Socket } from 'socket.io-client';
+
+interface SocketValue {
+  socket: Socket | undefined;
+}
+
+const SocketContext = createContext<SocketValue>({} as SocketValue);
+
+export const useSocketContext = () => useContext(SocketContext);
+
+interface Props {
+  children: ReactNode;
+}
+
+const UserInfoProvider: FC<Props> = ({ children }) => {
+  const socketRef = useRef<Socket>();
+
+  return (
+    <SocketContext.Provider value={{ socket: socketRef.current }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export default UserInfoProvider;

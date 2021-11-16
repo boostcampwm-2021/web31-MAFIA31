a2fa0874e227b7288e3bd40b300c155bbf7310d7
Subin Min <tnqls5417@naver.com>
2021-11-16 17:07:43 +0900

    feat: User 모델 구현

---
+++ packages/server/models/User.ts
@@ -0,0 +1,36 @@
+import { Document, model, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  userName: string;
+  profileImg: string;
+  score: number;
+  playCnt: number;
+  jobStat: {
+    [job: string]: {
+      killRate: number;
+      winRate: number;
+      cnt: number;
+      winCnt: number;
+      mvpCnt: number;
+    };
+  };
+}
+
+const schema = new Schema({
+  userName: { type: String, required: true },
+  profileImg: { type: String, required: true },
+  score: { type: Number, required: false, default: 0 },
+  playCnt: { type: Number, required: false, default: 0 },
+  jobStat: {
+    type: Object,
+    required: true,
+    default: {
+      mafia: { killRate: 0, winRate: 0, cnt: 0, winCnt: 0, mvpCnt: 0 },
+      citizen: { killRate: 0, winRate: 0, cnt: 0, winCnt: 0, mvpCnt: 0 },
+    },
+  },
+});
+
+const User = model<IUser>('User', schema);
+
+export default User;

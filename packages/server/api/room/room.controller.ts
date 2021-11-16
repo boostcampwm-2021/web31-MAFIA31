e4192c43a8ad479bae3fd9fab1ecd6e26f776b49
Subin Min <tnqls5417@naver.com>
2021-11-16 17:07:23 +0900

    fix: RoomController error시에도 res 보내도록 수정
    - catch 문 내 res.json() 추가

--- packages/server/api/room/room.controller.ts
+++ packages/server/api/room/room.controller.ts
@@ -8,16 +8,17 @@ const RoomController = {
       res.json({ roomList });
     } catch (error) {
       console.log(error);
+      res.json({ error });
     }
   },
   async addRoom(req: express.Request, res: express.Response) {
     try {
       const newRoom: IRoom = req.body;
       await Room.create(newRoom);
-
       res.json(newRoom);
     } catch (error) {
       console.log(error);
+      res.json({ error });
     }
   },
 };

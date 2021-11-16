edd3c57736b3bc29878c8018f7980aa2e72146f0
Subin Min <tnqls5417@naver.com>
2021-11-16 18:37:08 +0900

    fix: Room 모델 수정
    - host 컬럼 삭제

--- packages/server/models/Room.ts
+++ packages/server/models/Room.ts
@@ -3,13 +3,11 @@ import { Document, model, Schema } from 'mongoose';
 export interface IRoom extends Document {
   roomId: string;
   title: string;
-  host: string;
 }
 
 const schema = new Schema({
   roomId: { type: String, required: true },
   title: { type: String, required: true },
-  host: { type: String, required: true },
 });
 
 const Room = model<IRoom>('Room', schema);

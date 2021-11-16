f6df99248df72197f1dc18e6061ef13a90b13b20
donggoolosori <kdj6551@naver.com>
2021-11-16 17:41:53 +0900

    feat: 헤더 로고 변경

--- packages/client/src/components/Icon/index.tsx
+++ packages/client/src/components/Icon/index.tsx
@@ -3,3 +3,4 @@ export { ReactComponent as RoomOutIcon } from './room-out.svg';
 export { ReactComponent as VoteIcon } from './vote.svg';
 export { ReactComponent as SendIcon } from './send.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as LogoIcon } from './logo.svg';

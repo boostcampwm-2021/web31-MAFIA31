eccb3f335c7eec5fb95e6ad0f853701c0d992230
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-16 18:38:11 +0900

    feat: RoleCard 구현

--- packages/client/src/components/Card/RoleCard.tsx
+++ packages/client/src/components/Card/RoleCard.tsx
@@ -1,3 +1,28 @@
-const RoleCard = () => <div />;
+import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { JOB_DICT } from '@src/constants/job';
+import { Image, ImageSizeList } from '../Image';
+
+interface Props {
+  job: string;
+}
+
+const RoleCard: FC<Props> = ({ job }) => (
+  <div css={RoleCardStyle}>
+    <div>{JOB_DICT[job].title}</div>
+    <Image size={ImageSizeList.SMALL} src={JOB_DICT[job].imageSrc} />
+    <div>{JOB_DICT[job].description}</div>
+  </div>
+);
+
+const RoleCardStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px 0px;
+  text-align: center;
+`;
 
 export default RoleCard;

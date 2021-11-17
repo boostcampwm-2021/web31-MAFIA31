008278e4634baed3c402d324f5a395dc55434f79
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-17 19:01:19 +0900

    chore: story에 아무도 마피아에 의해 죽지 않은 경우 추가

--- packages/client/src/constants/story.ts
+++ packages/client/src/constants/story.ts
@@ -1,12 +1,16 @@
 import { StoryDictionary } from '@src/types';
 
 export const STORY_DICTIONARY: StoryDictionary = {
-  EXECUTED: {
-    msg: (name) => `투표로 ${name}이 죽었어요!`,
-    imgSrc: 'assets/images/mafia-kill.png',
+  EXECUTION: {
+    msg: (name) => `투표로 ${name}이(가) 죽었어요!`,
+    imgSrc: '/assets/images/die-vote.png',
   },
-  KILLED: {
-    msg: (name) => `탕! ${name}이 죽었어요!`,
-    imgSrc: 'assets/images/mafia-kill.png',
+  PUBLISH_VICTIM: {
+    msg: (name) => `탕! ${name}이(가) 죽었어요!`,
+    imgSrc: '/assets/images/mafia-kill.png',
+  },
+  NO_KILL: {
+    msg: () => `오늘 밤은 총소리가 들리지 않았어요!`,
+    imgSrc: '/assets/images/citizen.png',
   },
 };

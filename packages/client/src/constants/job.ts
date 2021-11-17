dfe0ecf4f9f2467ef5af77f241d866fa3357c86f
Kim Hyunjo <63906230+Kim-Hyunjo@users.noreply.github.com>
2021-11-17 10:20:25 +0900

    [TASK] 게임페이지 직업 설명 모달 구현 (#108)
    * chore: 필요한 이미지 추가
    
    * fix: JOB_DICT에 imgSrc 추가
    
    * feat: Modal 컴포넌트
    
    * feat: RoleCard 구현
    
    * fix: Modal css 수정
    
    * fix: 리뷰 반영
    
    - 카멜 케이스
    - 함수명 변경 (handleClick)
    
    Co-authored-by: Dongjune Kim <53213397+donggoolosori@users.noreply.github.com>

--- packages/client/src/constants/job.ts
+++ packages/client/src/constants/job.ts
@@ -2,6 +2,7 @@ interface JobDictionary {
   [job: string]: {
     title: string;
     description: string;
+    imageSrc: string;
   };
 }
 
@@ -9,21 +10,26 @@ export const JOB_DICT: JobDictionary = {
   '': {
     title: 'loading',
     description: 'loading',
+    imageSrc: 'assets/images/user.png',
   },
   'mafia': {
     title: '마피아',
     description: '밤에 사람을 한 명 죽일 수 있습니다.',
+    imageSrc: 'assets/images/mafia.png',
   },
   'citizen': {
     title: '시민',
     description: '아무것도 아닌 선량한 시민입니다.',
+    imageSrc: 'assets/images/citizen.png',
   },
   'doctor': {
     title: '의사',
     description: '밤에 사람을 한 명 살릴 수 있습니다.',
+    imageSrc: 'assets/images/doctor.png',
   },
   'police': {
     title: '경찰',
     description: '밤에 임의의 한 명을 선택해 마피아인지 확인할 수 있습니다.',
+    imageSrc: 'assets/images/police.png',
   },
 };

05bbe46d664921f5b26142f0b4a6e5a520f272e2
J078_민수빈 <tnqls5417@naver.com>
2021-11-18 20:22:50 +0900

    [TASK] 유저 정보 조회 API 구현 (#127)
    * feat: 사용자 정보 반환 위한 UserService 추가
    
    - findOne() User 모델에서 score, playCnt, jobStat json 형태로 반환
    
    * feat: 사용자 조회 API 라우팅 및 controller 추가
    
    - req.params 통해서 파라미터 받기
    
    * fix: 비동기 로직 처리 후 결과 반환하도록 수정
    
    - 기존 res 먼저 보내고 로직 처리
    - Promise.all()로 모두 처리후 res 전송
    
    * fix: API 경로 수정
    
    - GET /api/rooms
    - POST /api/users/stat
    - GET /api/users/:userName
    
    * refactor: 사용자 정보 모두 보내주도록 변경
    
    - User 스키마 형태로 반환

--- packages/server/api/user/index.ts
+++ packages/server/api/user/index.ts
@@ -3,6 +3,7 @@ import UserController from './user.controller';
 
 const userRouter = express.Router();
 
-userRouter.post('/update', UserController.updateUser);
+userRouter.post('/stat', UserController.updateUsersStat);
+userRouter.get('/:userName', UserController.getUser);
 
 export default userRouter;

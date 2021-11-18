efdf2846a54469604d642712b953e9cc48aa8f13
dailyco <dailyco03@gmail.com>
2021-11-18 16:41:07 +0900

    refactor: 헤더 버튼 간격 수정 및 스타일 리팩토링

--- packages/client/src/templates/Header/index.tsx
+++ packages/client/src/templates/Header/index.tsx
@@ -3,16 +3,15 @@ import { css } from '@emotion/react';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { DefaultButton, ButtonSizeList, ButtonThemeList } from '@src/components/Button';
-import { primaryDark, white } from '@src/constants';
+import { primaryDark } from '@src/constants';
 import { LightLogoIcon } from '@src/components/Icon';
 
-// const TITLE = 'MAFIA31';
 const CREATE_ROOM_BUTTON = '방 만들기';
 const MY_PAGE_BUTTON = '마이 페이지';
 
 const Header: FC = () => (
   <header css={headerStyle}>
-    <Link to="rooms">
+    <Link to="/rooms">
       <LightLogoIcon />
     </Link>
     <div css={buttonWrapperStyle}>
@@ -36,20 +35,16 @@ const headerStyle = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 40px;
-
-  height: 100px;
-
   font-weight: bold;
-  font-size: 50px;
-  line-height: 72px;
-  color: ${white};
+
+  padding: 40px;
+  height: 100px;
   background-color: ${primaryDark};
 `;
 
 const buttonWrapperStyle = css`
   display: flex;
-  gap: 10px;
+  gap: 24px;
 `;
 
 export default Header;

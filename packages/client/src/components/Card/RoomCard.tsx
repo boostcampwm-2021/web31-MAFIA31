c90e1cdb8acec1dfa02aa1b52c9612f2c7d18e09
dailyco <dailyco03@gmail.com>
2021-11-18 16:34:24 +0900

    refactor: RoomCard 컴포넌트 UI 리팩토링

--- packages/client/src/components/Card/RoomCard.tsx
+++ packages/client/src/components/Card/RoomCard.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { RoomInfo } from '@src/types';
-import { primaryDark, white } from '@src/constants';
+import { primaryDark, titleActive } from '@src/constants';
 
 interface Prop {
   roomInfo: RoomInfo;
@@ -13,64 +13,77 @@ const RoomCard: FC<Prop> = ({ roomInfo }) => {
   const userCount: number = 3;
 
   return (
-    <section css={roomCardStyle}>
+    <div css={roomCardStyle}>
       <div css={leftSide}>
-        <div className="room__title">{title}</div>
-        <div>
-          <span className="room__user-count">{`${userCount} / 12`}</span>
+        <div className="room-title">{title}</div>
+        <div className="room-info">
+          <span css={roomOption}>
+            <img src="/assets/icons/person.svg" alt="person" />
+            {`${userCount} / 12`}
+          </span>
         </div>
       </div>
-      <div css={rightSide}>
-        <img src="/assets/icons/person.svg" alt="person" />
-        <span className="room__user-count">{`${userCount} / 12`}</span>
-      </div>
-    </section>
+      <div css={rightSide} />
+    </div>
   );
 };
 
 const roomCardStyle = css`
-  display: flex;
+  position: relative;
 
-  max-width: 565px;
-  height: 220px;
+  padding: 24px;
+  width: 450px;
+  height: 180px;
   border-radius: 20px;
-  color: ${primaryDark};
+  color: ${titleActive};
   box-shadow: 2px 2px 8px rgba(78, 65, 109, 0.25);
 
   :hover {
     opacity: 0.8;
   }
-
-  .room__user-count {
-    font-weight: 500;
-    font-size: 16px;
-  }
 `;
 
 const leftSide = css`
-  padding: 24px;
-  width: 440px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
-  .room__title {
+  width: 302px;
+  height: 100%;
+
+  .room-title {
     font-weight: bold;
-    font-size: 30px;
-    line-height: 35px;
+    font-size: 24px;
+  }
+
+  .room-info {
+    display: flex;
+    align-items: center;
   }
 `;
 
 const rightSide = css`
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  width: 100px;
+  height: 180px;
+  border-radius: 0 20px 20px 0;
+  background-color: ${primaryDark};
+`;
+
+const roomOption = css`
   display: flex;
-  justify-content: center;
   align-items: center;
 
-  width: 125px;
-  color: ${white};
-  background-color: ${primaryDark};
-  border-top-right-radius: 20px;
-  border-bottom-right-radius: 20px;
+  gap: 5px;
+  font-size: 14px;
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
 `;
 
 export default RoomCard;

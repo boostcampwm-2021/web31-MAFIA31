fcf97e8297e0707d65adc597a6d6278c0593b7bb
donggoolosori <kdj6551@naver.com>
2021-11-18 14:16:00 +0900

    feat: 서버 의사가 살린 생존자 발표 이벤트 추가
    - 마피아가 정한 희생자와 의사가 살린 사람이 같을 경우 publish victim 대신 publish survivor 이벤트 전송

--- packages/server/sockets/ability.ts
+++ packages/server/sockets/ability.ts
@@ -1,19 +1,28 @@
-import { MAFIA_ABILITY, POLICE_INVESTIGATION, PUBLISH_VICTIM } from '@mafia/domain/constants/event';
+import * as EVENT from '@mafia/domain/constants/event';
 import { PoliceInvestigation } from '@mafia/domain/types/game';
 import { Namespace, Socket } from 'socket.io';
 import GameStore from '../stores/GameStore';
 
 const VictimStore: Record<string, string> = {};
+const SurvivorStore: Record<string, string> = {};
 
 const publishVictim = (namespace: Namespace) => {
   const roomId = namespace.name;
   const victim = VictimStore[roomId];
-  namespace.emit(PUBLISH_VICTIM, victim);
-  const newGameInfoList = GameStore.get(roomId).map((player) =>
-    player.userName === victim ? { ...player, isDead: true } : player,
-  );
-  GameStore.set(roomId, newGameInfoList);
+  const survivor = SurvivorStore[roomId];
+
+  if (victim === survivor) {
+    namespace.emit(EVENT.PUBLISH_SURVIVOR, survivor);
+  } else {
+    namespace.emit(EVENT.PUBLISH_VICTIM, victim);
+    const newGameInfoList = GameStore.get(roomId).map((player) =>
+      player.userName === victim ? { ...player, isDead: true } : player,
+    );
+    GameStore.set(roomId, newGameInfoList);
+  }
+
   VictimStore[roomId] = '';
+  SurvivorStore[roomId] = '';
 };
 
 const abilitySocketInit = (socket: Socket) => {
@@ -21,17 +30,22 @@ const abilitySocketInit = (socket: Socket) => {
   const roomId = namespace.name;
   VictimStore[roomId] = '';
 
-  socket.on(MAFIA_ABILITY, (mafiaPick: string) => {
+  socket.on(EVENT.MAFIA_ABILITY, (mafiaPick: string) => {
     VictimStore[roomId] = mafiaPick;
-    namespace.to('mafia').emit(MAFIA_ABILITY, VictimStore[roomId]);
+    namespace.to('mafia').emit(EVENT.MAFIA_ABILITY, VictimStore[roomId]);
   });
 
-  socket.on(POLICE_INVESTIGATION, (userName: string) => {
+  socket.on(EVENT.POLICE_INVESTIGATION, (userName: string) => {
     const isMafia: boolean =
       GameStore.get(namespace.name).find((el) => el.userName === userName)?.job === 'mafia';
 
     const resData: PoliceInvestigation = { userName, isMafia };
-    namespace.to('police').emit(POLICE_INVESTIGATION, resData);
+    namespace.to('police').emit(EVENT.POLICE_INVESTIGATION, resData);
+  });
+
+  socket.on(EVENT.DOCTOR_ABILITY, (userName: string) => {
+    console.log(userName);
+    SurvivorStore[roomId] = userName;
   });
 };
 

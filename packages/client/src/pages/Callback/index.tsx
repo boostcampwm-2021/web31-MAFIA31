c3447e657ab71bdbb81c9277560736d5fb97b2a3
Subin Min <tnqls5417@naver.com>
2021-11-23 00:35:02 +0900

    feat: 초대 링크 기능 위해서 로그인 완료시 이전 url로 이동
    - 기존 로그인: /(/login) -> /callback -> /(/login)
    - 초대 링크: /waiting?uuid -> /callback -> /waiting?uuid

--- packages/client/src/pages/Callback/index.tsx
+++ packages/client/src/pages/Callback/index.tsx
@@ -1,15 +1,17 @@
 import { useUserInfo } from '@src/contexts/userInfo';
 import { useEffect } from 'react';
-import { Redirect, useLocation } from 'react-router-dom';
+import { Redirect, useLocation, useHistory } from 'react-router-dom';
 
 const Callback = () => {
   const { setUserInfo } = useUserInfo();
+  const history = useHistory();
 
   const getUserData = async (code: string) => {
     const url = `${process.env.REACT_APP_API_URL}/api/auth/user?code=${code}`;
     const response = await fetch(url, { credentials: 'include' });
     const userData = await response.json();
     setUserInfo(userData);
+    history.goBack();
   };
   const searchParams = new URLSearchParams(useLocation().search);
   const codeStr = searchParams.get('code');
@@ -20,7 +22,7 @@ const Callback = () => {
   }, []);
   // context, JWT cookie 설정
 
-  return <Redirect to="/rooms" />;
+  return <div>loading</div>;
 };
 
 export default Callback;

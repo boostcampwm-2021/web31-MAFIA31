e7fa9dfa5f04d378eb29cbe20d4f039f00779e00
dailyco <dailyco03@gmail.com>
2021-11-22 17:56:10 +0900

    feat: Room API 수정 및 생성
    - room update API 생성
    - room get API 생성
    - room add API 수정

--- packages/server/api/room/index.ts
+++ packages/server/api/room/index.ts
@@ -4,6 +4,8 @@ import RoomController from './room.controller';
 const roomRouter = express.Router();
 
 roomRouter.get('/', RoomController.getRoomList);
+roomRouter.get('/:roomId', RoomController.getRoom);
 roomRouter.post('/', RoomController.addRoom);
+roomRouter.put('/', RoomController.updateRoomStatus);
 
 export default roomRouter;
--- packages/server/api/room/room.controller.ts
+++ packages/server/api/room/room.controller.ts
@@ -11,11 +11,53 @@ const RoomController = {
       res.status(400).json({ error });
     }
   },
-  async addRoom(req: express.Request, res: express.Response) {
+  async getRoom(req: express.Request, res: express.Response) {
+    if (!req.params.roomId) {
+      res.status(400).json({
+        error: 'Request not include roomId',
+      });
+      return;
+    }
+
     try {
-      const newRoom: IRoom = req.body;
+      const roomId: string = req.params.roomId;
+      const room = await Room.findOne({ roomId });
+      res.status(200).json({ room });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error });
+    }
+  },
+  async addRoom(req: express.Request, res: express.Response) {
+    if (!req.body.newRoom) {
+      res.status(400).json({
+        error: 'Request not include newRoom',
+      });
+      return;
+    }
+
+    try {
+      const newRoom: IRoom = req.body.newRoom;
       await Room.create(newRoom);
       res.status(200).json(newRoom);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error });
+    }
+  },
+  async updateRoomStatus(req: express.Request, res: express.Response) {
+    if (!req.body.roomId || !req.body.status) {
+      res.status(400).json({
+        error: 'Request not include roomId or status',
+      });
+      return;
+    }
+
+    try {
+      const roomId: string = req.body.roomId;
+      const status: string = req.body.status;
+      await Room.updateOne({ roomId }, { status });
+      res.status(200).json({ result: 'Success' });
     } catch (error) {
       console.log(error);
       res.status(400).json({ error });

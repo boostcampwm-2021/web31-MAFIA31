1f4a2be8b873660d1ae24d40dd74a72c71279467
Dongjune Kim <53213397+donggoolosori@users.noreply.github.com>
2021-11-22 20:45:54 +0900

    [TASK] Express 에러 핸들링 (#148)
    * chore: rest client 파일 gitignore 추가
    
    * feat: 커스텀 에러의 기본 부모 클래스 추가
    
    * feat: 커스텀 internal error 클래스 추가
    
    * feat: InvalidRoomIdError 클래스 추가
    
    - 이 오류는 유저 요청 데이터의 roomId가 유효하지 않을 때 발생
    
    * feat: 에러 핸들러 추가
    
    - 받은 에러의 상태코드값과 메시지를 응답해준다.
    
    * feat: addRoom 컨트롤러에서 roomId의 유효성 검사 및 에러 처리

--- packages/server/api/room/room.controller.ts
+++ packages/server/api/room/room.controller.ts
@@ -1,5 +1,20 @@
 import express from 'express';
+import InternalServerError from '../../error/InternalServerError';
+import InvalidRoomIdError from '../../error/InvalidRoomIdError';
 import Room, { IRoom } from '../../models/Room';
+
+const checkRoomId = (roomId: string): boolean => {
+  if (!roomId) {
+    return false;
+  }
+
+  const regex = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
+  if (!regex.test(roomId)) {
+    return false;
+  }
+
+  return true;
+};
 
 const RoomController = {
   async getRoomList(req: express.Request, res: express.Response) {
@@ -7,18 +22,25 @@ const RoomController = {
       const roomList = await Room.find();
       res.status(200).json({ roomList });
     } catch (error) {
-      console.log(error);
       res.status(400).json({ error });
     }
   },
-  async addRoom(req: express.Request, res: express.Response) {
+  async addRoom(req: express.Request, res: express.Response, next: express.NextFunction) {
     try {
       const newRoom: IRoom = req.body;
+
+      if (!checkRoomId(newRoom.roomId)) {
+        throw new InvalidRoomIdError();
+      }
+
       await Room.create(newRoom);
       res.status(200).json(newRoom);
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ error });
+      if (error instanceof InvalidRoomIdError) {
+        next(error);
+      }
+
+      next(new InternalServerError());
     }
   },
 };

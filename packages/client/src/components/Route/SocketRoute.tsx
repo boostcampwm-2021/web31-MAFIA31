888a42c1d5c7b88c86aa6d3de01be0756ccac2a7
Subin Min <tnqls5417@naver.com>
2021-11-22 21:40:56 +0900

    feat: 소켓 라우트 추가
    - socket disconnect 필요 경로 props로 받아서 route 이전에 처리

---
+++ packages/client/src/components/Route/SocketRoute.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import { Route } from 'react-router-dom';
+import disconnectSocket from '@src/utils/disconnectSocket';
+
+interface Prop {
+  exact?: boolean;
+  path: string;
+  component: any;
+  socket?: boolean;
+}
+
+const SocketRoute: FC<Prop> = ({ exact = false, path, component, socket = false }) => {
+  if (!socket) {
+    disconnectSocket();
+  }
+
+  return <Route exact={exact} path={path} component={component} />;
+};
+export default SocketRoute;

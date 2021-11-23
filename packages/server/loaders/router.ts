42245ef9507591d3798c5a2ec9bbc32be60f96c0
Subin Min <tnqls5417@naver.com>
2021-11-23 18:36:00 +0900

    fix: 로그인 후 이동 경로 수정
    - 루트로 이동
    - /api/auth 라우트 인증 미들웨어 제거

--- packages/server/loaders/router.ts
+++ packages/server/loaders/router.ts
@@ -5,8 +5,8 @@ import userRouter from '../api/user';
 import authMiddleware from '../middlewares/auth';
 
 const routerLoader = (app: express.Application) => {
-  app.use(authMiddleware);
   app.use('/api/auth', authRouter);
+  app.use(authMiddleware);
   app.use('/api/rooms', roomRouter);
   app.use('/api/users', userRouter);
 };

050001a99dd95df9e1a7feb24defefd011d466ae
kimhyunjo <alsxmqkqh23@gmail.com>
2021-11-23 16:09:04 +0900

    feat: 투표, 마피아 능력으로 사망한 사람 없을 경우 스토리 카드 추가

--- packages/client/src/constants/story.ts
+++ packages/client/src/constants/story.ts
@@ -6,6 +6,10 @@ export const STORY_DICTIONARY: StoryDictionary = {
     imgSrc: '/assets/images/vote-kill.png',
     type: 'execution',
   },
+  NO_VOTE: {
+    msg: () => `오늘은 처형식이 없는 날이네요!`,
+    imgSrc: '/assets/images/citizen.png',
+  },
   PUBLISH_VICTIM: {
     msg: (name) => `탕! ${name}이(가) 죽었어요!`,
     imgSrc: '/assets/images/mafia-kill.mp4',

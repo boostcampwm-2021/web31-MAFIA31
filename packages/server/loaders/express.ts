aac46ac8400098500202e006161c516c5d789804
Subin Min <tnqls5417@naver.com>
2021-11-25 12:03:20 +0900

    fix: 소켓 서버 apiClient 수정
    - 소켓 URL CORS 및 크로스 도메인 쿠키 공유 허용
    - 소켓 요청에서 origin 헤더 추가
    - config/url.config.json에 socketURL 추가

--- packages/server/loaders/express.ts
+++ packages/server/loaders/express.ts
@@ -1,13 +1,28 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import { clientURL } from '../config/url.config.json';
+import logger from 'morgan';
+import { clientURL, socketURL } from '../config/url.config.json';
 
 const expressLoader = (app: express.Application) => {
   const PORT: string = process.env.PORT || '5000';
+
   app.use(cookieParser());
   app.use(express.json());
-  app.use(cors({ origin: clientURL, credentials: true }));
+  app.use(logger('dev'));
+  const whiteList = [clientURL, socketURL];
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (whiteList.includes(origin!)) {
+          callback(null, true);
+        } else {
+          callback(null, false);
+        }
+      },
+      credentials: true,
+    }),
+  );
   app.listen(PORT, () =>
     console.log(`✅ Express Server Listening on : http://localhost:${PORT} 🚀`),
   );

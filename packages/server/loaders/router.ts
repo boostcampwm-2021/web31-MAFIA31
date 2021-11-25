c0b3118883710c77830d03999ba3dd397baedd1b
J078_민수빈 <tnqls5417@naver.com>
2021-11-25 19:41:46 +0900

    [FIX] 소켓에서의 API 요청 수정 (#180)
    * chore: 로거 의존성 추가
    
    - morgan @types/morgan 의존성 추가
    
    * fix: 소켓 서버 apiClient 수정
    
    - 소켓 URL CORS 및 크로스 도메인 쿠키 공유 허용
    - 소켓 요청에서 origin 헤더 추가
    - config/url.config.json에 socketURL 추가
    
    * feat: 소켓 - 클라이언트 credential 설정 추가
    
    - 소켓 인증 구현시 대비
    
    * refactor: 필요없는 콘솔 코드 삭제
    
    * fix: API 요청 apiClient 사용하도록 수정
    
    * feat: 존재하지 않는 API 예외처리
    
    - 405 MethodNotAllowedError로 처리

--- packages/server/loaders/router.ts
+++ packages/server/loaders/router.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import authRouter from '../api/auth';
 import roomRouter from '../api/room';
 import userRouter from '../api/user';
+import MethodNotAllowedError from '../error/MethodNotAllowedError';
 import authMiddleware from '../middlewares/auth';
 
 const routerLoader = (app: express.Application) => {
   app.use('/api/auth', authRouter);
-  app.use(authMiddleware);
-  app.use('/api/rooms', roomRouter);
-  app.use('/api/users', userRouter);
+  app.use('/api/rooms', authMiddleware, roomRouter);
+  app.use('/api/users', authMiddleware, userRouter);
+  app.use('*', (req: express.Request, res: express.Response, next: express.NextFunction) =>
+    next(new MethodNotAllowedError('Requested API not exist. Check request url.')),
+  );
 };
 
 export default routerLoader;

3da67748bc98f684ab856eb68ebdd07ce68d502e
J078_민수빈 <tnqls5417@naver.com>
2021-11-29 20:10:06 +0900

    feat: 소켓 서버 패키지로 분리 (#206)
    Co-authored-by: donggoolosori <kdj6551@naver.com>

--- packages/server/loaders/index.ts
+++ packages/server/loaders/index.ts
@@ -3,13 +3,11 @@ import dbLoader from './db';
 import errorHandlerLoader from './errorHandlerLoader';
 import expressLoader from './express';
 import routerLoader from './router';
-import socketLoader from './socket';
 
 const initLoader = (app: express.Application) => {
   dbLoader();
   expressLoader(app);
   routerLoader(app);
-  socketLoader(app);
   errorHandlerLoader(app);
 };
 
